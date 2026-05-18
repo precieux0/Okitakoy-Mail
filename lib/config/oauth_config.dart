7e50b7edcf3f2339b148648e5933c5e87b3afed3
precieux0 <jmakumbusela@gmail.com>
2026-05-18 11:32:29 +0100

    update oauth

--- lib/config/oauth_config.dart
+++ lib/config/oauth_config.dart
@@ -1,6 +1,16 @@
 class OAuthConfig {
+  // Google
   static const googleWebClientId =
       '114601066380-4cfloq6ldgrrsi8j9odmp830er6g4rui.apps.googleusercontent.com';
 
-  static const callbackScheme = 'okitakoymail';
+  // GitHub - lues depuis les dart-defines passés au build
+  static final String githubClientId =
+      const String.fromEnvironment('GITHUB_CLIENT_ID', defaultValue: '');
+
+  static final String githubClientSecret =
+      const String.fromEnvironment('GITHUB_CLIENT_SECRET', defaultValue: '');
+
+  // Redirection URI : basée sur le schéma personnalisé
+  static const String callbackScheme = 'okitakoymail';
+  static String get githubRedirectUri => '$callbackScheme://callback';
 }
